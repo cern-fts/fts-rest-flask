68a5d900ad40b53a17e8910984201de9aa3804f8
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2020-02-17 13:30:08 +0100

    Migrate meta, remove base

---
+++ src/fts3rest/fts3rest/model/meta.py
@@ -0,0 +1,28 @@
+#   Copyright  Members of the EMI Collaboration, 2013.
+#   Copyright 2020 CERN
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""SQLAlchemy Metadata and Session object"""
+import logging
+
+import sqlalchemy
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from fts3.model.base import Base  # The declarative Base
+
+# SQLAlchemy session manager. Updated by model.init_model()
+Session = scoped_session(sessionmaker())
+
+# Log the version
+logging.getLogger(__name__).info("Using SQLAlchemy %s" % sqlalchemy.__version__)
