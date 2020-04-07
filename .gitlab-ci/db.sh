190069e3dabd1fd4b5a3feac4a4b41df0448b8f9
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2020-04-07 13:15:12 +0200

    fix

--- .gitlab-ci/db.sh
+++ .gitlab-ci/db.sh
@@ -4,4 +4,4 @@ curl -O https://gitlab.cern.ch/fts/fts3/-/raw/fts-oidc-integration/src/db/schema
 curl -O https://gitlab.cern.ch/fts/fts3/-/raw/fts-oidc-integration/src/db/schema/mysql/fts-diff-6.0.0.sql
 mysql --user=root --password=asdf --host=mariadb ftsflask < fts-schema-5.0.0.sql
 mysql --user=root --password=asdf --host=mariadb ftsflask < fts-diff-6.0.0.sql
-mysql --user=root --password=asdf --host=mariadb ftsflask < .gitlab-ci/db.sql
+mysql --user=root --password=asdf --host=mariadb  < .gitlab-ci/db.sql
