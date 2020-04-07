968dfa75c2703c67767100a57f8039937c021165
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2020-04-07 13:30:09 +0200

    test

--- .gitlab-ci/db.sh
+++ .gitlab-ci/db.sh
@@ -2,7 +2,9 @@
 
 curl -O https://gitlab.cern.ch/fts/fts3/-/raw/fts-oidc-integration/src/db/schema/mysql/fts-schema-5.0.0.sql
 curl -O https://gitlab.cern.ch/fts/fts3/-/raw/fts-oidc-integration/src/db/schema/mysql/fts-diff-6.0.0.sql
-echo "CREATE DATABASE ftsflask;" | mysql --user=root --password=asdf --host=mariadb
+
 mysql --user=root --password=asdf --host=mariadb ftsflask < fts-schema-5.0.0.sql
 mysql --user=root --password=asdf --host=mariadb ftsflask < fts-diff-6.0.0.sql
-mysql --user=root --password=asdf --host=mariadb  < .gitlab-ci/db.sql
+#mysql --user=root --password=asdf --host=mariadb  < .gitlab-ci/db.sql
+echo "CREATE USER 'ftsflask'@'mariadb';" | mysql --user=root --password=asdf --host=mariadb
+echo "GRANT ALL PRIVILEGES ON ftsflask.* TO 'ftsflask'@'mariadb' IDENTIFIED BY 'asdf';" | mysql --user=root --password=asdf --host=mariadb
