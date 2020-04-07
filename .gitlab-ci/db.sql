94559c6ab9e4afbb5e9eece72ef4a8e17464e010
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2020-04-07 13:03:37 +0200

    fix

--- .gitlab-ci/db.sql
+++ .gitlab-ci/db.sql
@@ -1,2 +1,2 @@
-CREATE USER 'ftsflask'@'centos__mariadb';
-GRANT ALL PRIVILEGES ON ftsflask.* To 'ftsflask'@'centos_mariadb' IDENTIFIED BY 'asdf';
+CREATE USER 'ftsflask'@'mariadb';
+GRANT ALL PRIVILEGES ON ftsflask.* To 'ftsflask'@'mariadb' IDENTIFIED BY 'asdf';
