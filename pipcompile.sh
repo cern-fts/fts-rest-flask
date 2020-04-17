be1908979e695fbbad780007fa72e578a29d8b27
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2020-04-17 14:01:02 +0200

    ignore datamanagement tests in ci

--- pipcompile.sh
+++ pipcompile.sh
@@ -1,6 +1,3 @@
-# this assumes the venv is created with --system-site-packages
-# so the gfal2-python3 rpm can be used
-# here we use --ignore-installed to ensure all other packages are installed in the venv
-pip-compile --generate-hashes requirements.in --pip-args '--ignore-installed'
-pip-compile --generate-hashes dev-requirements.in --pip-args '--ignore-installed'
+pip-compile --generate-hashes requirements.in --pip-args 
+pip-compile --generate-hashes dev-requirements.in --pip-args
 
