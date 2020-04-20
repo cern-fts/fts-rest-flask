52cdb8bcfebb4891f2a620d9159bdd150e50cd18
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2020-04-20 14:24:55 +0200

    migrate config init

--- src/fts3rest/fts3rest/controllers/config/__init__.py
+++ src/fts3rest/fts3rest/controllers/config/__init__.py
@@ -13,59 +13,13 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-import json
-import logging
-from flask import request
-from fts3.model import *
-from fts3rest.model.meta import Session
-from fts3rest.lib.middleware.fts3auth.authorization import authorize
-from fts3rest.lib.middleware.fts3auth.constants import CONFIG
-from werkzeug.exceptions import NotFound
-
-log = logging.getLogger(__name__)
-
-
-def audit_configuration(action, config):
-    """
-    Logs and stores in the DB a configuration action
-    """
-    audit = ConfigAudit(
-        datetime=datetime.utcnow(),
-        dn=request.environ["fts3.User.Credentials"].user_dn,
-        config=config,
-        action=action,
-    )
-    Session.add(audit)
-    log.info(action)
-
-
-#   Copyright notice:
-#
-#   Copyright CERN 2013-2015
-#
-#   Copyright Members of the EMI Collaboration, 2013.
-#       See www.eu-emi.eu for details on the copyright holders
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
-import json
 import logging
 from numbers import Number
 from flask import request
 
 from fts3.model import *
 from fts3rest.model.meta import Session
-from fts3rest.lib.helpers import accept
+from fts3rest.lib.helpers.accept import accept
 from werkzeug.exceptions import BadRequest
 from fts3rest.lib.middleware.fts3auth.authorization import authorize
 from fts3rest.lib.middleware.fts3auth.constants import CONFIG
