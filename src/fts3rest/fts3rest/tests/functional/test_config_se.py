9c38cf0c05a1ab63efd84644a3d3188ef48f3a7d
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2020-04-22 14:15:49 +0000

    Resolve FTS-1609

---
+++ src/fts3rest/fts3rest/tests/functional/test_config_se.py
@@ -0,0 +1,163 @@
+from fts3rest.tests import TestController
+from fts3rest.model.meta import Session
+from fts3.model import ConfigAudit, Optimizer, OperationConfig, Se
+
+
+class TestConfigSe(TestController):
+    def setUp(self):
+        super(TestConfigSe, self).setUp()
+        self.setup_gridsite_environment()
+        Session.query(Optimizer).delete()
+        Session.query(ConfigAudit).delete()
+        Session.query(OperationConfig).delete()
+        Session.query(Se).delete()
+        Session.commit()
+        self.host_config = {
+            "operations": {
+                "atlas": {"delete": 22, "staging": 32,},
+                "dteam": {"delete": 10, "staging": 11},
+            },
+            "se_info": {
+                "ipv6": 1,
+                "outbound_max_active": 55,
+                "inbound_max_active": 11,
+                "inbound_max_throughput": 33,
+                "se_metadata": "metadata",
+            },
+        }
+
+    def tearDown(self):
+        Session.query(Optimizer).delete()
+        Session.query(ConfigAudit).delete()
+        Session.query(OperationConfig).delete()
+        Session.query(Se).delete()
+        Session.commit()
+        super(TestConfigSe, self).tearDown()
+
+    def test_set_se_config(self):
+        """
+        Set SE config
+        """
+        config = {"test.cern.ch": self.host_config}
+        self.app.post_json("/config/se", params=config, status=200)
+
+        audits = Session.query(ConfigAudit).all()
+        self.assertEqual(2, len(audits))
+
+        ops = (
+            Session.query(OperationConfig)
+            .filter(OperationConfig.host == "test.cern.ch")
+            .all()
+        )
+        self.assertEqual(4, len(ops))
+        for op in ops:
+            self.assertEqual(
+                config[op.host]["operations"][op.vo_name][op.operation],
+                op.concurrent_ops,
+            )
+
+        se = Session.query(Se).filter(Se.storage == "test.cern.ch").first()
+        self.assertEqual(1, se.ipv6)
+        self.assertEqual(55, se.outbound_max_active)
+        self.assertEqual(11, se.inbound_max_active)
+        self.assertEqual(33, se.inbound_max_throughput)
+
+    def test_set_se_config_bad_name(self):
+        """
+        Set SE config with invalid storage names
+        """
+        for storage_name in (" ", "", "\n", "\t"):
+            config = {storage_name: self.host_config}
+
+            self.app.post_json("/config/se", params=config, status=400)
+
+            audits = Session.query(ConfigAudit).all()
+            self.assertEqual(0, len(audits))
+
+            ops = (
+                Session.query(OperationConfig)
+                .filter(OperationConfig.host == storage_name)
+                .all()
+            )
+            self.assertEqual(0, len(ops))
+
+            se = Session.query(Se).filter(Se.storage == storage_name).all()
+            self.assertEqual(0, len(se))
+
+    def test_reset_se_config(self):
+        """
+        Reset SE config
+        """
+        self.test_set_se_config()
+
+        config = {
+            "test.cern.ch": {
+                "operations": {
+                    "atlas": {"delete": 1, "staging": 2,},
+                    "dteam": {"delete": 3, "staging": 4},
+                },
+                "se_info": {
+                    "ipv6": 0,
+                    "outbound_max_active": 88,
+                    "inbound_max_active": 11,
+                    "inbound_max_throughput": 10,
+                },
+            }
+        }
+        self.app.post_json("/config/se", params=config, status=200)
+
+        audits = Session.query(ConfigAudit).all()
+        self.assertEqual(4, len(audits))
+
+        ops = (
+            Session.query(OperationConfig)
+            .filter(OperationConfig.host == "test.cern.ch")
+            .all()
+        )
+        self.assertEqual(4, len(ops))
+        for op in ops:
+            self.assertEqual(
+                config[op.host]["operations"][op.vo_name][op.operation],
+                op.concurrent_ops,
+            )
+
+        se = Session.query(Se).filter(Se.storage == "test.cern.ch").first()
+        self.assertEqual(0, se.ipv6)
+        self.assertEqual(88, se.outbound_max_active)
+        self.assertEqual(11, se.inbound_max_active)
+        self.assertEqual(10, se.inbound_max_throughput)
+
+    def test_get_se_config(self):
+        """
+        Get SE config
+        """
+        self.test_set_se_config()
+
+        cfg = self.app.get_json("/config/se?se=test.cern.ch", status=200).json
+
+        self.assertIn("test.cern.ch", cfg.keys())
+        se_cfg = cfg["test.cern.ch"]
+
+        self.assertIn("operations", se_cfg.keys())
+        self.assertIn("se_info", se_cfg.keys())
+
+        self.assertEqual(
+            {
+                "atlas": {"delete": 22, "staging": 32},
+                "dteam": {"delete": 10, "staging": 11},
+            },
+            se_cfg["operations"],
+        )
+
+        self.assertEqual(1, se_cfg["se_info"]["ipv6"])
+        self.assertEqual(55, se_cfg["se_info"]["outbound_max_active"])
+        self.assertEqual(11, se_cfg["se_info"]["inbound_max_active"])
+        self.assertEqual(33, se_cfg["se_info"]["inbound_max_throughput"])
+
+    def test_remove_se_config(self):
+        """
+        Remove the configuration for a given SE
+        """
+        self.test_get_se_config()
+        self.app.delete(url="/config/se", status=400)
+        self.app.delete(url="/config/se?se=test.cern.ch", status=204)
---
+++ src/fts3rest/fts3rest/tests/functional/test_config_shares.py
@@ -0,0 +1,123 @@
+from fts3rest.tests import TestController
+from fts3rest.model.meta import Session
+from fts3.model.config import (
+    ConfigAudit,
+    ServerConfig,
+    OperationConfig,
+    ShareConfig,
+    LinkConfig,
+)
+
+
+class TestConfigShares(TestController):
+    def setUp(self):
+        super(TestConfigShares, self).setUp()
+        self.setup_gridsite_environment()
+        Session.query(ShareConfig).delete()
+        Session.query(LinkConfig).delete()
+        Session.query(ServerConfig).delete()
+        Session.query(ConfigAudit).delete()
+        Session.query(OperationConfig).delete()
+        Session.commit()
+
+    def tearDown(self):
+        Session.query(ShareConfig).delete()
+        Session.query(LinkConfig).delete()
+        Session.query(ServerConfig).delete()
+        Session.query(ConfigAudit).delete()
+        Session.query(OperationConfig).delete()
+        Session.commit()
+        super(TestConfigShares, self).tearDown()
+
+    def test_set_share(self):
+        """
+        Set up shares config
+        """
+        Session.execute(
+            "INSERT INTO "
+            "t_link_config(source_se, dest_se, symbolic_name,"
+            " min_active, max_active, optimizer_mode, nostreams)"
+            " VALUES('gsiftp://source', 'gsiftp://nowhere', '*', 2, 130, 2, 0);"
+        )
+        self.app.post_json(
+            url="/config/shares",
+            params=dict(
+                source="gsiftp://source",
+                destination="gsiftp://nowhere",
+                vo="dteam",
+                share=80,
+            ),
+            status=200,
+        )
+
+    def test_wrong_config_shares0(self):
+        """
+        Test wrong value for share in params
+        """
+        self.app.post_json(
+            url="/config/shares",
+            params=dict(
+                source="gsiftp://source",
+                destination="gsiftp://nowhere",
+                vo="dteam",
+                share="dfdf",
+            ),
+            status=400,
+        )
+
+    def test_wrong_config_shares1(self):
+        """
+        Test missing one of params
+        """
+        config = {
+            "source": "gsiftp://source",
+            "destination": "gsiftp://nowhere",
+            "vo": "dteam",
+            "share": 80,
+        }
+
+        for i in config:
+            k = config
+            k[i] = ""
+            self.app.post_json(url="/config/shares", params=k, status=400)
+            return config
+
+    def test_wrong_config_shares2(self):
+        """
+        Test wrong source or destination
+        """
+        self.app.post_json(
+            url="/config/shares",
+            params=dict(
+                source="dfgsdfsg", destination="gsiftp://nowhere", vo="dteam", share=80
+            ),
+            status=400,
+        )
+
+        self.app.post_json(
+            url="/config/shares",
+            params=dict(
+                source="gsiftp://source", destination="klhjkhjk", vo="dteam", share=80
+            ),
+            status=400,
+        )
+
+    def test_get_share_config(self):
+        """
+        Get shares config
+        """
+        self.test_set_share()
+        self.app.get(url="/config/shares", status=200)
+
+    def test_remove_share(self):
+        """
+        Try to remove shares
+        """
+        self.app.delete(
+            url="/config/shares?share=80&destination=gsiftp://nowhere&vo=dteam",
+            status=400,
+        )
+        self.app.delete(
+            url="/config/shares?share=80&destination=gsiftp://nowhere&vo=dteam&source=gsiftp://source",
+            status=204,
+        )
