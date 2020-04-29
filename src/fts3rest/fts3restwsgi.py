7ad45a3dab6f12f9965abdaabe12237adebc705a
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2020-04-29 09:46:30 +0200

    fix

--- src/fts3rest/fts3restwsgi.py
+++ src/fts3rest/fts3restwsgi.py
@@ -2,6 +2,6 @@ from fts3rest.config.middleware import create_app
 
 # default_config_filename = '/etc/fts3/fts3config'
 default_config_filename = (
-    "/home/ftsflask/fts-rest-flask/src/fts3rest/fts3rest/tests/fts3config"
+    "/home/ftsflask/fts-rest-flask/src/fts3rest/fts3rest/tests/fts3testconfig"
 )
 application = create_app(default_config_filename)
