2c0c89aae94f13dcf645f865a201227b4c9dc1ef
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2020-04-29 16:13:31 +0200

    Remove best match

--- src/fts3rest/fts3rest/lib/helpers/accept.py
+++ src/fts3rest/fts3rest/lib/helpers/accept.py
@@ -12,10 +12,8 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from werkzeug.exceptions import NotAcceptable
-from fts3rest.lib.helpers.jsonify import to_json
 import functools
-from flask import request, Response
+from flask import request
 from fts3rest.templates.mako import render_template
 import logging
 
@@ -24,37 +22,19 @@ from flask import current_app as app
 
 
 def accept(html_template):
-    """
-    Depending on the Accept headers returns a different representation of the data
-    returned by the decorated method
-    """
-    offers = [("text/html", 1.1), ("application/json", 1)]
     # todo: maybe we have to lstrip "/" from template name if it doesnt work
     def accept_inner(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            try:
-                best_match = request.accept_mimetypes.best_match(
-                    offers, default="application/json"
-                )
-            except Exception:
-                best_match = "application/json"
-            log.debug("best_match {}".format(best_match))
-            if not best_match:
-                raise NotAcceptable("Available: %s" % ", ".join(offers))
-
             data = func(*args, **kwargs)
-            if best_match == "text/html":
-                return render_template(
-                    html_template,
-                    **{
-                        "data": data,
-                        "config": app.config,
-                        "user": request.environ["fts3.User.Credentials"],
-                    },
-                )
-            else:
-                return Response(to_json(data), mimetype="application/json")
+            return render_template(
+                html_template,
+                **{
+                    "data": data,
+                    "config": app.config,
+                    "user": request.environ["fts3.User.Credentials"],
+                },
+            )
 
         return wrapper
 
