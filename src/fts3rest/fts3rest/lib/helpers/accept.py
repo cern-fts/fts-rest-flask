ea49a9247f1674753098cd1f934e4f3544201ade
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2020-04-30 12:02:23 +0200

    Restore json in accept

--- src/fts3rest/fts3rest/lib/helpers/accept.py
+++ src/fts3rest/fts3rest/lib/helpers/accept.py
@@ -11,9 +11,11 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-
+from werkzeug.exceptions import NotAcceptable
+from fts3rest.lib.helpers.jsonify import to_json
 import functools
-from flask import request
+from flask import request, Response
+
 from fts3rest.templates.mako import render_template
 import logging
 
@@ -22,18 +24,35 @@ from flask import current_app as app
 
 
 def accept(html_template):
+    """
+    Depending on the Accept headers returns a different representation of the data
+    returned by the decorated method
+    """
+    offers = [("text/html", 1.1), ("application/json", 1)]
+
     def accept_inner(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
+            try:
+                best_match = request.accept_mimetypes.best_match(offers)
+            except Exception:
+                best_match = "application/json"
+            log.debug("best_match {}".format(best_match))
+            if not best_match:
+                raise NotAcceptable("Available: %s" % ", ".join(offers))
+
             data = func(*args, **kwargs)
-            return render_template(
-                html_template,
-                **{
-                    "data": data,
-                    "config": app.config,
-                    "user": request.environ["fts3.User.Credentials"],
-                },
-            )
+            if best_match == "text/html":
+                return render_template(
+                    html_template,
+                    **{
+                        "data": data,
+                        "config": app.config,
+                        "user": request.environ["fts3.User.Credentials"],
+                    },
+                )
+            else:
+                return Response(to_json(data), mimetype="application/json")
 
         return wrapper
 
