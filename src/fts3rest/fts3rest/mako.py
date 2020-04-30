027c22c527dd4c9ca7d9d3d6017dc26000e2f27c
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2020-04-30 10:55:15 +0200

    Move mako

--- src/fts3rest/fts3rest/mako.py
+++ src/fts3rest/fts3rest/mako.py
@@ -2,7 +2,9 @@
 from mako.lookup import TemplateLookup
 import tempfile
 
-mylookup = TemplateLookup(directories=["./"], module_directory=tempfile.mkdtemp())
+mylookup = TemplateLookup(
+    directories=["templates"], module_directory=tempfile.mkdtemp()
+)
 
 # todo: are static files found?
 def render_template(template_name, **context):
