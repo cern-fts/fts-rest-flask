509e26367f113fb176d970f7f965964da59afb50
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2020-05-18 13:26:57 +0200

    fix

--- packaging/fts-rest-client.sh
+++ packaging/fts-rest-client.sh
@@ -1,9 +1,9 @@
 PREPDIR=~/fts-rest-client-0.1
-mkdir $PREPDIR
+mkdir -p $PREPDIR
 cp ../LICENSE $PREPDIR
 cp ../setup.py $PREPDIR
 cp -r ../src/cli ../src/fts3 $PREPDIR
+cp fts-rest-client.spec ~/rpmbuild/SPECS
 cd
 tar -czf fts-rest-client-0.1.tar.gz fts-rest-client-0.1
 mv fts-rest-client-0.1.tar.gz rpmbuild/SOURCES/
-cp packaging/fts-rest-client.spec rpmbuild/SPECS
