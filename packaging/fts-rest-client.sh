71505711a7af98c41a7bb26083db7d8c02055816
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2020-05-18 13:24:25 +0200

    Add rpm preparation script

--- packaging/fts-rest-client.sh
+++ packaging/fts-rest-client.sh
@@ -3,3 +3,7 @@ mkdir $PREPDIR
 cp ../LICENSE $PREPDIR
 cp ../setup.py $PREPDIR
 cp -r ../src/cli ../src/fts3 $PREPDIR
+cd
+tar -czf fts-rest-client-0.1.tar.gz fts-rest-client-0.1
+mv fts-rest-client-0.1.tar.gz rpmbuild/SOURCES/
+cp packaging/fts-rest-client.spec rpmbuild/SPECS
