1f548db0e24d9fa08da15ecefc3dc1df56a342ec
Carles Garcia Cabot <carles.garcia.cabot@cern.ch>
2020-05-19 16:31:56 +0200

    fix ci

--- packaging/fts-rest-server.sh
+++ packaging/fts-rest-server.sh
@@ -1,7 +1,7 @@
 PREPDIR=~/fts-rest-server-0.1
 mkdir -p $PREPDIR
 cp ../LICENSE $PREPDIR
-cp -r ../src/ftsrest $PREPDIR
+cp -r ../src/fts3rest $PREPDIR
 cp fts-rest-server.spec ~/rpmbuild/SPECS
 cd
 tar -czf fts-rest-server-0.1.tar.gz fts-rest-server-0.1
