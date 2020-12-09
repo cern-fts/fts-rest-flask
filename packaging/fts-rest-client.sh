91c2dc8fdf27205832b7442197d36d962c2c16e0
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2020-12-09 17:24:49 +0100

    Refactor helper build scripts: - Automatically extract version from SPEC file - Build the tarball in a temporary directory instead of the home folder

--- packaging/fts-rest-client.sh
+++ packaging/fts-rest-client.sh
@@ -1,10 +1,30 @@
-PREPDIR=~/fts-rest-client-1.0
-mkdir -p $PREPDIR/src
-cp ../LICENSE $PREPDIR
-cp ../setup.py $PREPDIR
-cp -r ../src/cli ../src/fts3 $PREPDIR/src
+#!/bin/sh
+set -e
+
+# -------------------------------------------------------------------
+# Utility script to help packaging the fts-rest-client.
+# The script must be called from the packaging directory
+# and expects an rpmdev tree already exists
+# -------------------------------------------------------------------
+
+# Setup
+TMPDIR=$(mktemp -d /tmp/fts-rest-client-build_XXXXX)
+VERSION=$(grep -m1 fts-rest-client.spec -e "^Version:" | awk {'print $2'})
+DISTDIR=${TMPDIR}/fts-rest-client-${VERSION}
+mkdir -p ${DISTDIR}/src
+echo "Version: ${VERSION}"
+
+# Copying files for dist
+cp ../LICENSE ${DISTDIR}
+cp ../setup.py ${DISTDIR}
+cp -r ../src/cli ../src/fts3 ${DISTDIR}/src
+
+# Create tarball
+tar -C ${TMPDIR} -czf fts-rest-client-${VERSION}.tar.gz fts-rest-client-${VERSION}/
+
+# Place files in
+mv fts-rest-client-${VERSION}.tar.gz ~/rpmbuild/SOURCES/
 cp fts-rest-client.spec ~/rpmbuild/SPECS
-cd
-tar -czf fts-rest-client-1.0.tar.gz fts-rest-client-1.0
-mv fts-rest-client-1.0.tar.gz rpmbuild/SOURCES/
-cd ~/rpmbuild/SPECS
+
+rm -rf ${TMPDIR}
+echo "Created tarball:" ~/rpmbuild/SOURCES/fts-rest-client-${VERSION}.tar.gz
--- packaging/fts-rest-server.sh
+++ packaging/fts-rest-server.sh
@@ -1,9 +1,29 @@
-PREPDIR=~/fts-rest-server-1.0
-mkdir -p $PREPDIR
-cp ../LICENSE $PREPDIR
-cp -r ../src/fts3rest $PREPDIR
+#!/bin/sh
+set -e
+
+# -------------------------------------------------------------------
+# Utility script to help packaging the fts-rest-server.
+# The script must be called from the packaging directory
+# and expects an rpmdev tree already exists
+# -------------------------------------------------------------------
+
+# Setup
+TMPDIR=$(mktemp -d /tmp/fts-rest-server-build_XXXXX)
+VERSION=$(grep -m1 fts-rest-server.spec -e "^Version:" | awk {'print $2'})
+DISTDIR=${TMPDIR}/fts-rest-server-${VERSION}
+mkdir -p ${DISTDIR}
+echo "Version: ${VERSION}"
+
+# Copying files for dist
+cp ../LICENSE ${DISTDIR}
+cp -r ../src/fts3rest ${DISTDIR}
+
+# Create tarball
+tar -C ${TMPDIR} -czf fts-rest-server-${VERSION}.tar.gz fts-rest-server-${VERSION}/
+
+# Copy spec file to rpmbuild
+mv fts-rest-server-${VERSION}.tar.gz ~/rpmbuild/SOURCES/
 cp fts-rest-server.spec ~/rpmbuild/SPECS
-cd
-tar -czf fts-rest-server-1.0.tar.gz fts-rest-server-1.0
-mv fts-rest-server-1.0.tar.gz rpmbuild/SOURCES/
-cd ~/rpmbuild/SPECS
+
+rm -rf ${TMPDIR}
+echo "Created tarball: " ~/rpmbuild/SOURCES/fts-rest-server-${VERSION}.tar.gz
