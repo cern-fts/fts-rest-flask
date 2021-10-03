57644db84c3a93e8f735eb301c642214a652cbfd
Joao Lopes <joao.pedro.batista.lopes@cern.ch>
2021-10-03 23:27:07 +0200

    FTS-1731: Update db schema to the latest fts-schema-6.0.0.sql

--- .gitlab-ci/db.sh
+++ .gitlab-ci/db.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -O https://gitlab.cern.ch/fts/fts3/-/raw/fts-oidc-integration/src/db/schema/mysql/fts-schema-6.0.0.sql
+curl -O https://gitlab.cern.ch/fts/fts3/-/raw/master/src/db/schema/mysql/fts-schema-6.0.0.sql
 
 mysql --user=root --password=asdf --host=mariadb ftsflask < fts-schema-6.0.0.sql
 echo "CREATE USER 'ci'@'%';" | mysql --user=root --password=asdf --host=mariadb
