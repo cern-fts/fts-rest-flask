b01f1fa4ad9bcca6a10d7f9c32ccdba1148a58f5
Joao Lopes <joao.pedro.batista.lopes@cern.ch>
2021-11-26 15:25:15 +0100

    Fetch latest database schema

--- .gitlab-ci/db.sh
+++ .gitlab-ci/db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-curl -O https://gitlab.cern.ch/fts/fts3/-/raw/master/src/db/schema/mysql/fts-schema-7.0.0.sql
+curl -O https://gitlab.cern.ch/fts/fts3/-/raw/FTS-1744/src/db/schema/mysql/fts-schema-8.0.0.sql
 
-mysql --user=root --password=asdf --host=mariadb ftsflask < fts-schema-7.0.0.sql
+mysql --user=root --password=asdf --host=mariadb ftsflask < fts-schema-8.0.0.sql
 echo "CREATE USER 'ci'@'%';" | mysql --user=root --password=asdf --host=mariadb
 echo "GRANT ALL PRIVILEGES ON ftsflask.* TO 'ci'@'%' IDENTIFIED BY 'asdf';" | mysql --user=root --password=asdf --host=mariadb
