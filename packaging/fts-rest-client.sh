74f22dff5e323961c65b7aa8e8f0f30d58cdde62
Joao Lopes <joao.pedro.batista.lopes@cern.ch>
2022-01-31 09:01:13 +0100

    CI: Extract release version from branch name when building rpms

--- packaging/fts-rest-client.sh
+++ packaging/fts-rest-client.sh
@@ -7,25 +7,53 @@ set -e
 # and expects an rpmdev tree already exists
 # -------------------------------------------------------------------
 
+function print_info {
+  printf "======================\n"
+  printf "Branch:\t\t%s\n" "${BRANCH}"
+  printf "Release:\t%s\n" "${RELEASE}"
+  printf "Version:\t%s\n" "${VERSION}"
+  printf "======================\n"
+}
+
 # Setup
 TMPDIR=$(mktemp -d /tmp/fts-rest-client-build_XXXXX)
-VERSION=$(grep -m1 fts-rest-client.spec -e "^Version:" | awk {'print $2'})
+VERSION=$(grep -m1 packaging/fts-rest-client.spec -e "^Version:" | awk {'print $2'})
+TIMESTAMP=`date +%y%m%d%H%M`
+GITREF=`git rev-parse --short HEAD`
+RELEASE=r${TIMESTAMP}git${GITREF}
 DISTDIR=${TMPDIR}/fts-rest-client-${VERSION}
+
+if [[ -z ${BRANCH} ]]; then
+  BRANCH=`git name-rev $GITREF --name-only`
+else
+  printf "Using environment set variable BRANCH=%s\n" "${BRANCH}"
+fi
+
+if [[ $BRANCH =~ ^(tags/)?(v)[.0-9]+(-(rc)?([0-9]+))?$ ]]; then
+  RELEASE="${BASH_REMATCH[4]}${BASH_REMATCH[5]}"
+fi
+
+print_info
+
 mkdir -p ${DISTDIR}/src
-echo "Version: ${VERSION}"
 
 # Copying files for dist
-cp ../LICENSE ${DISTDIR}
-cp ../setup.py ${DISTDIR}
-cp ../setup.cfg ${DISTDIR}
-cp -r ../src/cli ../src/fts3 ${DISTDIR}/src
+cp LICENSE ${DISTDIR}
+cp setup.py ${DISTDIR}
+cp setup.cfg ${DISTDIR}
+cp -r src/cli src/fts3 ${DISTDIR}/src
 
 # Create tarball
 tar -C ${TMPDIR} -czf fts-rest-client-${VERSION}.tar.gz fts-rest-client-${VERSION}/
 
 # Place files in
 mv fts-rest-client-${VERSION}.tar.gz ~/rpmbuild/SOURCES/
-cp fts-rest-client.spec ~/rpmbuild/SPECS
+cp packaging/fts-rest-client.spec ~/rpmbuild/SPECS
 
 rm -rf ${TMPDIR}
 echo "Created tarball:" ~/rpmbuild/SOURCES/fts-rest-client-${VERSION}.tar.gz
+
+cd ~/rpmbuild/SPECS
+
+rpmbuild -bs --define "_release ${RELEASE}" fts-rest-client.spec
+rpmbuild -bb --define "_release ${RELEASE}" fts-rest-client.spec
--- packaging/fts-rest-server.sh
+++ packaging/fts-rest-server.sh
@@ -7,23 +7,51 @@ set -e
 # and expects an rpmdev tree already exists
 # -------------------------------------------------------------------
 
+function print_info {
+  printf "======================\n"
+  printf "Branch:\t\t%s\n" "${BRANCH}"
+  printf "Release:\t%s\n" "${RELEASE}"
+  printf "Version:\t%s\n" "${VERSION}"
+  printf "======================\n"
+}
+
 # Setup
 TMPDIR=$(mktemp -d /tmp/fts-rest-server-build_XXXXX)
-VERSION=$(grep -m1 fts-rest-server.spec -e "^Version:" | awk {'print $2'})
+VERSION=$(grep -m1 packaging/fts-rest-server.spec -e "^Version:" | awk {'print $2'})
+TIMESTAMP=`date +%y%m%d%H%M`
+GITREF=`git rev-parse --short HEAD`
+RELEASE=r${TIMESTAMP}git${GITREF}
 DISTDIR=${TMPDIR}/fts-rest-server-${VERSION}
-mkdir -p ${DISTDIR}
-echo "Version: ${VERSION}"
+
+if [[ -z ${BRANCH} ]]; then
+  BRANCH=`git name-rev $GITREF --name-only`
+else
+  printf "Using environment set variable BRANCH=%s\n" "${BRANCH}"
+fi
+
+if [[ $BRANCH =~ ^(tags/)?(v)[.0-9]+(-(rc)?([0-9]+))?$ ]]; then
+  RELEASE="${BASH_REMATCH[4]}${BASH_REMATCH[5]}"
+fi
+
+print_info
+
+mkdir -p ${DISTDIR}/src
 
 # Copying files for dist
-cp ../LICENSE ${DISTDIR}
-cp -r ../src/fts3rest ${DISTDIR}
+cp LICENSE ${DISTDIR}
+cp -r src/fts3rest ${DISTDIR}
 
 # Create tarball
 tar -C ${TMPDIR} -czf fts-rest-server-${VERSION}.tar.gz fts-rest-server-${VERSION}/
 
 # Copy spec file to rpmbuild
 mv fts-rest-server-${VERSION}.tar.gz ~/rpmbuild/SOURCES/
-cp fts-rest-server.spec ~/rpmbuild/SPECS
+cp packaging/fts-rest-server.spec ~/rpmbuild/SPECS
 
 rm -rf ${TMPDIR}
 echo "Created tarball: " ~/rpmbuild/SOURCES/fts-rest-server-${VERSION}.tar.gz
+
+cd ~/rpmbuild/SPECS
+
+rpmbuild -bs --define "_release ${RELEASE}" fts-rest-server.spec
+rpmbuild -bb --define "_release ${RELEASE}" fts-rest-server.spec
