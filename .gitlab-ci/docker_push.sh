9998d455c54a2cf708cb2bea63d62fcefde0c1f9
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2022-03-24 15:40:10 +0100

    Update Dockerfile to conform with recent versions of "pyenv"

--- .gitlab-ci/docker_push.sh
+++ .gitlab-ci/docker_push.sh
@@ -2,12 +2,12 @@
 
 # These files are needed for the docker build
 echo Building fts-rest-flask:ci ...
-cp ../pipcompile.sh ../pipsyncdev.sh ../dev-requirements.in ../requirements.in .
+cp ../pipcompile.sh ../pipsyncdev.sh ../requirements.in ../dev-requirements.in .
 docker login gitlab-registry.cern.ch/fts/fts-rest-flask
 docker build -t gitlab-registry.cern.ch/fts/fts-rest-flask:ci .
-docker push  gitlab-registry.cern.ch/fts/fts-rest-flask:ci
-rm pipcompile.sh pipsyncdev.sh dev-requirements.in requirements.in
+docker push gitlab-registry.cern.ch/fts/fts-rest-flask:ci
+rm pipcompile.sh pipsyncdev.sh requirements.in dev-requirements.in
 
 echo Building fts-rest-flask:rpm ...
-docker build  -t gitlab-registry.cern.ch/fts/fts-rest-flask:rpm . -f ./Dockerfile-rpm
-docker push  gitlab-registry.cern.ch/fts/fts-rest-flask:rpm
+docker build -t gitlab-registry.cern.ch/fts/fts-rest-flask:rpm . -f ./Dockerfile-rpm
+docker push gitlab-registry.cern.ch/fts/fts-rest-flask:rpm
