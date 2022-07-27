9b1d219e2537723ba457db6f1cfb81b8e8a4c3fb
Joao Lopes <joao.pedro.batista.lopes@cern.ch>
2022-07-27 17:13:25 +0200

    Only store JSON objects in the DB if the their value is different than None
      - The json.dumps(..) function returns the string "null" if the provided argument is of type None
      - Before calling json.dumps(..) we should check if the argument is of type None to avoid storing the string "null" in the database when nothing should be stored

--- src/fts3rest/fts3rest/model/base.py
+++ src/fts3rest/fts3rest/model/base.py
@@ -66,7 +66,10 @@ class Json(TypeDecorator):
         self.length = length
 
     def process_bind_param(self, value, dialect):
-        return json.dumps(value)
+        if value is None:
+            return None
+        else:
+            return json.dumps(value)
 
     def process_result_value(self, value, dialect):
         if value is None:
