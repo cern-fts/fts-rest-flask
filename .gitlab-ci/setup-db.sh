448db781f6813701fa1e069ef9b9105216ccb258
Joao Lopes <joao.pedro.batista.lopes@cern.ch>
2023-09-01 08:55:49 +0200

    CI: Use mysql service instaed of mariadb to run the functional tests
      This change was triggered when the gitlab shared runners infrastructure moved from docker runners to K8s runners.
      With this change the MariaDB service was not able to start.
      Since the MariaDB image being used before was very old (+5 years that it has not been updated) and since FTS runs MySQL in production these pipelines are changed to use from now on MySQL

--- .gitlab-ci/setup-db.sh
+++ .gitlab-ci/setup-db.sh
@@ -3,7 +3,7 @@
 curl -s -O https://gitlab.cern.ch/fts/fts3/-/raw/develop/src/db/schema/mysql/fts-schema-8.0.1.sql
 curl -s -O https://gitlab.cern.ch/fts/fts3/-/raw/develop/src/db/schema/mysql/fts-diff-8.1.0.sql
 
-mysql --user=root --password=ftsflaskroot --host=mariadb ftsflask < fts-schema-8.0.1.sql
-mysql --user=root --password=ftsflaskroot --host=mariadb ftsflask < fts-diff-8.1.0.sql
-echo "CREATE USER 'fts3'@'%';" | mysql --user=root --password=ftsflaskroot --host=mariadb
-echo "GRANT ALL PRIVILEGES ON ftsflask.* TO 'fts3'@'%' IDENTIFIED BY 'ftsflaskpass';" | mysql --user=root --password=ftsflaskroot --host=mariadb
+mysql --user=root --password=ftsflaskroot --host=mysqldb ftsflask < fts-schema-8.0.1.sql
+mysql --user=root --password=ftsflaskroot --host=mysqldb ftsflask < fts-diff-8.1.0.sql
+echo "CREATE USER 'fts3'@'%';" | mysql --user=root --password=ftsflaskroot --host=mysqldb
+echo "GRANT ALL PRIVILEGES ON ftsflask.* TO 'fts3'@'%' IDENTIFIED BY 'ftsflaskpass';" | mysql --user=root --password=ftsflaskroot --host=mysqldb
