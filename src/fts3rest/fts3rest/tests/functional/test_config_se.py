35c76a4ed5777e63d733523ac09f5ce52167e7be
Joao Lopes <joao.pedro.batista.lopes@cern.ch>
2023-09-13 14:55:53 +0200

    Tests: Add test for SE configuration with empty se_info JSON and empty operations JSON

--- src/fts3rest/fts3rest/tests/functional/test_config_se.py
+++ src/fts3rest/fts3rest/tests/functional/test_config_se.py
@@ -167,3 +167,74 @@ class TestConfigSe(TestController):
         self.test_get_se_config()
         self.app.delete(url="/config/se", status=400)
         self.app.delete(url="/config/se?se=test.cern.ch", status=204)
+
+    def test_set_se_config_empty(self):
+        """
+        Set SE config with an empty se_info JSON;
+        Only the operations should be configured and the se configuration should not be created
+        """
+        config = {
+            "test.cern.ch": {
+                "operations": {
+                    "atlas": {
+                        "delete": 22,
+                        "staging": 32,
+                    },
+                    "dteam": {"delete": 10, "staging": 11},
+                },
+                "se_info": {},
+            }
+        }
+        self.app.post_json("/config/se", params=config, status=200)
+
+        audits = Session.query(ConfigAudit).all()
+        self.assertEqual(1, len(audits))
+
+        ops = (
+            Session.query(OperationConfig)
+            .filter(OperationConfig.host == "test.cern.ch")
+            .all()
+        )
+        self.assertEqual(4, len(ops))
+        for op in ops:
+            self.assertEqual(
+                config[op.host]["operations"][op.vo_name][op.operation],
+                op.concurrent_ops,
+            )
+
+        se = Session.query(Se).filter(Se.storage == "test.cern.ch").first()
+        self.assertIsNone(se)
+
+    def test_set_operations_config_empty(self):
+        """
+        Set SE config with an empty operations JSON;
+        Only the se_info should be configured and the operations configuration should not be created
+        """
+        config = {
+            "test.cern.ch": {
+                "operations": {},
+                "se_info": {
+                    "ipv6": 1,
+                    "outbound_max_active": 88,
+                    "inbound_max_active": 11,
+                    "inbound_max_throughput": 10,
+                },
+            }
+        }
+        self.app.post_json("/config/se", params=config, status=200)
+
+        audits = Session.query(ConfigAudit).all()
+        self.assertEqual(1, len(audits))
+
+        ops = (
+            Session.query(OperationConfig)
+            .filter(OperationConfig.host == "test.cern.ch")
+            .all()
+        )
+        self.assertEqual(0, len(ops))
+
+        se = Session.query(Se).filter(Se.storage == "test.cern.ch").first()
+        self.assertEqual(1, se.ipv6)
+        self.assertEqual(88, se.outbound_max_active)
+        self.assertEqual(11, se.inbound_max_active)
+        self.assertEqual(10, se.inbound_max_throughput)
