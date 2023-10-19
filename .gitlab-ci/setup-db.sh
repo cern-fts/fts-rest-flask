698087c4d770bb9271d8348a47e3d4610547955a
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2023-10-19 16:51:30 +0200

    Modify the database setup script to install the "fts-schema-8.2.0" schema from the "master" branch

--- .gitlab-ci/setup-db.sh
+++ .gitlab-ci/setup-db.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-curl -s -O https://gitlab.cern.ch/fts/fts3/-/raw/develop/src/db/schema/mysql/fts-schema-8.0.1.sql
-curl -s -O https://gitlab.cern.ch/fts/fts3/-/raw/develop/src/db/schema/mysql/fts-diff-8.1.0.sql
-curl -s -O https://gitlab.cern.ch/fts/fts3/-/raw/FTS-1829_scitags/src/db/schema/mysql/fts-diff-8.2.0.sql
+curl -s -O https://gitlab.cern.ch/fts/fts3/-/raw/master/src/db/schema/mysql/fts-schema-8.2.0.sql
 
-mysql --user=root --password=ftsflaskroot --host=mysqldb ftsflask < fts-schema-8.0.1.sql
-mysql --user=root --password=ftsflaskroot --host=mysqldb ftsflask < fts-diff-8.1.0.sql
-mysql --user=root --password=ftsflaskroot --host=mysqldb ftsflask < fts-diff-8.2.0.sql
+mysql --user=root --password=ftsflaskroot --host=mysqldb ftsflask < fts-schema-8.2.0.sql
 echo "CREATE USER 'fts3'@'%' IDENTIFIED BY 'ftsflaskpass';" | mysql --user=root --password=ftsflaskroot --host=mysqldb
 echo "GRANT ALL PRIVILEGES ON ftsflask.* TO 'fts3'@'%';" | mysql --user=root --password=ftsflaskroot --host=mysqldb
