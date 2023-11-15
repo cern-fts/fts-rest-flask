fc750ad5cca7b5f17285fc1a923d3806a6d40691
Joao Lopes <joao.pedro.batista.lopes@cern.ch>
2023-11-15 14:56:43 +0100

    CI: Create build template for server and client rpms

---
+++ packaging/fedora-packages.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ -f /usr/bin/dnf ]]; then
+  dnf install -y yum-utils dnf-plugins-core git rpm-build make tree which
+else
+  yum install -y yum-utils git rpm-build make tree which
+fi
