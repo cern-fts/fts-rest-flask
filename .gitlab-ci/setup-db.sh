bc33e30de8a3fb8d6612bcea4944d8cb69c4d090
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2024-05-23 18:01:36 +0200

    CI: Switch schema to install from "develop" branch

--- .gitlab-ci/setup-db.sh
+++ .gitlab-ci/setup-db.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -s -O https://gitlab.cern.ch/fts/fts3/-/raw/FTS-1925_token_project/src/db/schema/mysql/fts-schema-9.0.0.sql
+curl -s -O https://gitlab.cern.ch/fts/fts3/-/raw/develop/src/db/schema/mysql/fts-schema-9.0.0.sql
 mysql --user=root --password=ftsflaskroot --host=mysqldb ftsflask < fts-schema-9.0.0.sql
 echo "CREATE USER 'fts3'@'%' IDENTIFIED BY 'ftsflaskpass';" | mysql --user=root --password=ftsflaskroot --host=mysqldb
 echo "GRANT ALL PRIVILEGES ON ftsflask.* TO 'fts3'@'%';" | mysql --user=root --password=ftsflaskroot --host=mysqldb
